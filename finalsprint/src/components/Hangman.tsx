ece2eb4aaf569dc0b3fdb20709baa8612b062cae
Zachary Collier <zacharycollier36@protonmail.com>
2024-12-14 21:45:24 -0330

    H A N G M A N

--- finalsprint/src/components/Hangman.tsx
+++ finalsprint/src/components/Hangman.tsx
@@ -1,17 +1,183 @@
 import circuitTree from "../assets/PortfolioPics/tree-shape-of-straight-lines-like-a-computer-printed-circuit-svgrepo-com.svg";
 import "../styles/Hangman.css";
 import { useState, useRef, useEffect } from "react";
+import Button from "./Button";
+import { Link } from "react-router-dom";
+
+import Hangman0 from "../assets/HangmanPics/hangman-0.png";
+import Hangman1 from "../assets/HangmanPics/hangman-1.png";
+import Hangman2 from "../assets/HangmanPics/hangman-2.png";
+import Hangman3 from "../assets/HangmanPics/hangman-3.png";
+import Hangman4 from "../assets/HangmanPics/hangman-4.png";
+import Hangman5 from "../assets/HangmanPics/hangman-5.png";
+
+function Letters(props: { letter: string }) {
+  return <div className="letterbox">{props.letter}</div>;
+}
 
 function Hangman() {
   const wordList = [
-    "Hello",
-    "Goodbye",
-    "Temporal",
-    "Hilarity",
-    "Painless",
-    "Happiness",
-    "Upset",
+    "HELLO",
+    "GOODBYE",
+    "TEMPORAL",
+    "HILARITY",
+    "PAINLESS",
+    "HAPPY",
+    "UPSET",
   ];
+
+  const hangmanStateList = [
+    Hangman0,
+    Hangman1,
+    Hangman2,
+    Hangman3,
+    Hangman4,
+    Hangman5,
+  ];
+
+  const [currentWord, setCurrentWord] = useState("");
+
+  useEffect(() => {
+    //Select random word
+    if (currentWord === "") {
+      setCurrentWord(wordList[Math.floor(Math.random() * wordList.length)]);
+    }
+  }, [currentWord]);
+
+  const [hangmanAlertInfo, setHangmanAlertInfo] = useState({
+    type: "empty",
+    message: "",
+  });
+
+  const hangmanAlert = useRef<null | HTMLDivElement>(null);
+
+  useEffect(() => {
+    hangmanAlert.current!.innerHTML = `<p>${hangmanAlertInfo.message}</p>`;
+    if (hangmanAlertInfo.type === "empty") {
+      hangmanAlert.current!.style.background = "rgba(0,0,0,0)";
+    } else if (hangmanAlertInfo.type === "success") {
+      hangmanAlert.current!.style.background = "green";
+    } else {
+      hangmanAlert.current!.style.background = "red";
+    }
+
+    if (hangmanAlertInfo.type != "empty") {
+      setTimeout(() => {
+        setHangmanAlertInfo({ type: "empty", message: "" });
+      }, 5000);
+    }
+  }, [hangmanAlertInfo]);
+
+  const [hangmanState, setHangmanState] = useState(0);
+
+  const [wordState, setWordState] = useState<string[]>([]);
+
+  const [letterArray, setLetterArray] = useState<string[]>([]);
+
+  useEffect(() => {
+    // Resets word
+    if (currentWord != "") {
+      setWordState([]);
+      setLetterArray([]);
+      for (let i = 0; i < currentWord.length; ++i) {
+        setWordState((previous) => [...previous, "_"]);
+        setLetterArray((previous) => [...previous, currentWord[i]]);
+      }
+    }
+  }, [currentWord]);
+
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
+  const [currentLetter, setCurrentLetter] = useState("");
+
+  const [disableState, setDisableState] = useState(false);
+
+  const handleLetterChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCurrentLetter(e.target.value.toUpperCase());
+  };
+
+  const handleLetterSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    const validCharacters = /[A-Z]/g;
+    if (validCharacters.test(currentLetter)) {
+      if (guessedLetters.includes(currentLetter)) {
+        setHangmanAlertInfo({
+          type: "fail",
+          message: "Letter already guessed.",
+        });
+      } else {
+        setGuessedLetters((previous) => [...previous, currentLetter]);
+        if (currentWord.includes(currentLetter)) {
+          setHangmanAlertInfo({
+            type: "success",
+            message: "Correct!",
+          });
+          for (let i = 0; i < currentWord.length; ++i) {
+            if (letterArray[i].includes(currentLetter)) {
+              setWordState(() => [
+                ...wordState.slice(0, i),
+                currentLetter,
+                ...wordState.slice(i + 1),
+              ]);
+            }
+          }
+        } else if (hangmanState < 4) {
+          setHangmanAlertInfo({
+            type: "fail",
+            message: "Oof. Guessed wrong.",
+          });
+          setHangmanState(hangmanState + 1);
+        } else {
+          setHangmanAlertInfo({
+            type: "fail",
+            message: "GAME OVER.",
+          });
+          setHangmanState(5);
+          setDisableState(true);
+        }
+      }
+    } else {
+      setHangmanAlertInfo({
+        type: "fail",
+        message: "Please input a valid letter.",
+      });
+    }
+  };
+
+  useEffect(() => {
+    for (let i = 0; i < currentWord.length; ++i) {
+      for (let j = 0; j < guessedLetters.length; ++j) {
+        if (
+          letterArray[i].includes(guessedLetters[j]) &&
+          wordState[i].includes("_")
+        ) {
+          setWordState(() => [
+            ...wordState.slice(0, i),
+            guessedLetters[j],
+            ...wordState.slice(i + 1),
+          ]);
+        }
+      }
+    }
+    if (wordState.join("") === currentWord && currentWord != "") {
+      setHangmanAlertInfo({
+        type: "success",
+        message: "VICTORY!",
+      });
+      setDisableState(true);
+    }
+  }, [wordState]);
+
+  const handleReset = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setCurrentWord("");
+    setHangmanState(0);
+    setWordState([]);
+    setLetterArray([]);
+    setDisableState(false);
+    setCurrentLetter("");
+    setGuessedLetters([]);
+  };
 
   return (
     <main>
@@ -58,10 +224,36 @@ function Hangman() {
         </div>
         <div className="mainarticle">
           <div className="hangmanbox">
-            <div className="hangmanalert"></div>
-            <div className="hangmanimg"></div>
-            <div className="hangmanword"></div>
-            <div className="hangmancontrols"></div>
+            <div className="hangmanalert" ref={hangmanAlert}></div>
+            <div className="hangmanimg">
+              <img src={hangmanStateList[hangmanState]} alt="Hangman Image" />
+            </div>
+            <div className="hangmanword">
+              {wordState.map((letter, index) => (
+                <Letters letter={letter} key={index} />
+              ))}
+            </div>
+            <div className="hangmancontrols">
+              <form>
+                <label htmlFor="guess">Your Guess:</label>
+                <input
+                  type="text"
+                  maxLength={1}
+                  id="letterinput"
+                  name="letterinput"
+                  value={currentLetter}
+                  onChange={handleLetterChange}
+                  disabled={disableState}
+                />
+                <button disabled={disableState} onClick={handleLetterSubmit}>
+                  Guess
+                </button>
+                <button onClick={handleReset}>Retry</button>
+                <Link to="/hangman-code">
+                  <Button name="Code" />
+                </Link>
+              </form>
+            </div>
           </div>
         </div>
       </article>
